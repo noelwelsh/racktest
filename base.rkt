00e9e3c492b1cab81ff612705fa599c79c836860
Noel Welsh <noelwelsh@gmail.com>
2011-02-15 11:58:06 +0000

    Add all the basic framework and get it working.

--- base.rkt
+++ base.rkt
@@ -1,15 +1,21 @@
 #lang typed/racket
 
-(define-type Name (Option String))
+;; Base Types
+
+(define-type (Dict Key Value)
+  (U (HashTable Key Value) (Listof (Pair Key Value))))
+
+(define-type Name (Listof Symbol))
+(define-type Info (Dict Symbol Any))
 
 ;; Test Results
 
-(define-type Test-Result (U success skipped pending exn:fail:test))
+(define-type Test-Result (U success skipped pending exn:fail:test exn:fail))
 
 (struct: success ([name : Name]))
 (struct: skipped ([name : Name]))
 (struct: pending ([name : Name]))
-(struct: exn:fail:test exn:fail ([name : Name] [info : (HashTable Symbol Any)]))
+(struct: exn:fail:test exn:fail ([name : Name] [info : (Listof Info)]))
 
 ;; Test Listener
 
@@ -23,13 +29,48 @@
 (: current-test-listener (Parameterof Test-Listener))
 (define current-test-listener (make-parameter default-test-listener))
 
+;; Test Names
+
+(: current-test-name (Parameterof Name))
+(define current-test-name (make-parameter null))
+
+(define-syntax-rule (with-test-name name expr ...)
+  (parameterize ([current-test-name (cons (current-test-name))])
+    expr ...))
+
+;; Test Info
+
+(: current-test-info (Parameterof (Listof Info)))
+(define current-test-info (make-parameter null))
+
+(define-syntax-rule (with-test-info ([key value] ...) expr ...)
+  (parameterize ([current-test-info (cons (list (cons key value) ...)
+                                          (current-test-info))])
+    expr ...))
+
 ;; Utilities
 
-(define (fail [msg "Test failed"])
-  (exn:fail:test #f (hash) msg (current-continuation-marks)))
+(define (pass)
+  (success (current-test-name)))
+
+(: fail (case-lambda (-> Nothing) (String -> Nothing)))
+(define fail
+  (case-lambda:
+    [()
+     (fail "Test Failed")]
+    [([msg : String])
+     (raise
+      (exn:fail:test msg (current-continuation-marks) (current-test-name) (current-test-info)))]))
+
+(define-syntax-rule (lift expr ...)
+  (with-handlers ([exn:fail? (current-test-listener)])
+    expr ...
+    (pass)))
+
 
 (provide
  Name
+ Info
  
  Test-Result
  success
@@ -40,4 +81,12 @@
  default-test-listener
  current-test-listener
 
- fail)
+ current-test-name
+ with-test-name
+
+ current-test-info
+ with-test-info
+
+ pass
+ fail
+ lift)
--- check.rkt
+++ check.rkt
@@ -1,5 +1,11 @@
 #lang typed/racket
 
+(require "base.rkt")
+
 (define (check v1 v2)
-  (unless (equal? v1 v2)
-    (fail (format "~a not equal to ~a" v1 v2))))
+  (lift
+   (unless (equal? v1 v2)
+     (fail (format "~a not equal to ~a" v1 v2)))))
+
+(provide
+ check)
